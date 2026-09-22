e36ca7bc74ce58880253a78b056324d69a094a2d
clouden <clouden@syneforge.com>
2026-09-22 21:46:55 +0300

    reacting to incoming notifications

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import * as Notifications from 'expo-notifications';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import {
 	SafeAreaProvider,
 	initialWindowMetrics,
@@ -9,14 +10,27 @@ import {
 Notifications.setNotificationHandler({
 	handleNotification: async () => ({
 		shouldPlaySound: false,
-    shouldSetBadge: false,
-    shouldShowAlert: true,
+		shouldSetBadge: false,
+		shouldShowAlert: true,
 		shouldShowBanner: true,
-    shouldShowList: true,
+		shouldShowList: true,
 	}),
 });
 
 export default function RootLayout() {
+	useEffect(() => {
+		const subscription = Notifications.addNotificationResponseReceivedListener(
+			(response) => {
+				console.log('NOTIFICATION RECEIVED');
+				console.log(response.notification.request.content.data?.userName);
+			},
+		);
+
+		return () => {
+			subscription.remove();
+		};
+	}, []);
+
 	return (
 		<SafeAreaProvider initialMetrics={initialWindowMetrics}>
 			<StatusBar style="dark" />
