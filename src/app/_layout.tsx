b3294504d84f88a49c75926c90c8e5401042de65
clouden <clouden@syneforge.com>
2026-09-23 21:07:02 +0300

    sending push notifications

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,9 @@
+import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Button, Platform } from 'react-native';
 import {
 	SafeAreaProvider,
 	initialWindowMetrics,
@@ -17,7 +19,81 @@ Notifications.setNotificationHandler({
 	}),
 });
 
+const sendPushNotification = async (expoPushToken: string) => {
+	const message = {
+		to: expoPushToken,
+		sound: 'default',
+		title: 'Original Title',
+		body: 'And here is the body!',
+		data: { someData: 'goes here' },
+	};
+
+	await fetch('https://exp.host/--/api/v2/push/send', {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Accept-encoding': 'gzip, deflate',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(message),
+	});
+};
+
+const handleRegistrationError = (errorMessage: string) => {
+	alert(errorMessage);
+	throw new Error(errorMessage);
+};
+
+const registerForPushNotificationsAsync = async () => {
+	if (Platform.OS === 'android') {
+		await Notifications.setNotificationChannelAsync('default', {
+			name: 'default',
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: '#FF231F7C',
+		});
+	}
+
+	const { status: existingStatus } = await Notifications.getPermissionsAsync();
+	let finalStatus = existingStatus;
+	if (existingStatus !== 'granted') {
+		const { status } = await Notifications.requestPermissionsAsync();
+		finalStatus = status;
+	}
+	if (finalStatus !== 'granted') {
+		handleRegistrationError(
+			'Permission not granted to get push token for push notification!',
+		);
+		return;
+	}
+	const projectId =
+		Constants?.expoConfig?.extra?.eas?.projectId ??
+		Constants?.easConfig?.projectId;
+	if (!projectId) {
+		handleRegistrationError('Project ID not found');
+	}
+	try {
+		const pushTokenString = (
+			await Notifications.getExpoPushTokenAsync({
+				projectId,
+			})
+		).data;
+		console.log(pushTokenString);
+		return pushTokenString;
+	} catch (e: unknown) {
+		handleRegistrationError(`${e}`);
+	}
+};
+
 export default function RootLayout() {
+	const [expoPushToken, setExpoPushToken] = useState<string>('');
+
+	useEffect(() => {
+		registerForPushNotificationsAsync()
+			.then((token) => setExpoPushToken(token ?? ''))
+			.catch((error: any) => setExpoPushToken(`${error}`));
+	}, []);
+
 	useEffect(() => {
 		const subscriptionNotificationReceived =
 			Notifications.addNotificationReceivedListener((response) => {
@@ -40,7 +116,16 @@ export default function RootLayout() {
 	return (
 		<SafeAreaProvider initialMetrics={initialWindowMetrics}>
 			<StatusBar style="dark" />
-			<Stack />
+			<Stack
+				screenOptions={{
+					headerRight: () => (
+						<Button
+							title="Send push notification"
+							onPress={() => sendPushNotification(expoPushToken)}
+						/>
+					),
+				}}
+			/>
 		</SafeAreaProvider>
 	);
 }
